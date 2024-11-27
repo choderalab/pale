931d9592c9e41a54144e5578f7d1f76b2ebce40e
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2024-11-27 17:10:46 -0500

    boltz prediction script prototype

--- playground/README.md
+++ playground/README.md
@@ -4,3 +4,12 @@ snippets.
 
 We expect this directory to be removed once we have achieved some stability in the tools and
 structure that we want to achieve in this project.
+
+
+## Boltz prediction script
+`boltz_prediction.py` is a script convenient to generate `yaml` files that could be readily
+used with `boltz` to make structure predictions. For example you can run it using:
+
+```python
+python boltz_tyk2_prediction.py --protein-file protein.pdb --ligand-file ligands.sdf --protein-id MYPROTEIN --output-yaml MYPROTEIN_ligands.yaml
+```
---
+++ playground/boltz_prediction.py
@@ -0,0 +1,100 @@
+"""
+Script to predict tyk2 complex structures using boltz.
+
+It takes data from a local copy of the protein ligand benchmark repository
+hosted in https://github.com/openforcefield/protein-ligand-benchmark
+
+The purpose is to programatically build a YAML file that boltz can use to
+make predictions from.
+
+Meant to be used as a sanity check for boltz.
+
+TODOs:
+ * Make basic CLI that accepts paths to pdb/sdf files.
+ * Actually generate the YAML file.
+"""
+
+import argparse
+import yaml
+from Bio.Data.IUPACData import protein_letters_3to1
+from pdbfixer import PDBFixer
+from openff.toolkit import Molecule
+
+
+def sequence_from_pdb(pdb_path: str):
+    """
+    Extract the protein sequence from a PDB file. It uses Openmm pdbfixer to extract the
+    information.
+    """
+    fixer = PDBFixer(pdb_path)
+
+    # Get single-letter sequence from pdbfixer object
+    sequence_obj = fixer.sequences[0]  # get first sequence in fixer obj
+    sequence = []
+    for residue in sequence_obj.residues:
+        try:
+            sequence.append(protein_letters_3to1[residue.title()])
+        except KeyError:  # It should only fail with caps
+            print(f"Residue {residue} conversion failed. Continuing.")
+            continue
+    return sequence
+
+
+def ligands_data_from_sdf(sdf_file: str):
+    """
+    Generates a dictionary with the ligands data from an sdf file.
+
+    Ligands indices are the keys.
+    Ligands smiles are the values.
+
+    TODO: Use ligands names/ids instead of indices. There's an issue with boltz schema.
+    """
+    # Get smiles for ligands
+    ligands = Molecule.from_file(sdf_file)
+    smiles_data = {str(mol_index): molecule.to_smiles(explicit_hydrogens=False) for
+                   mol_index, molecule in enumerate(ligands)}
+    return smiles_data
+
+
+def generate_boltz_yaml(protein_sequence: list, ligands_data: dict, protein_id: str, output_yaml: str):
+    """
+    Writes a YAML file that can be used by boltz, from protein sequence and ligands data
+    information.
+
+    Note: The resulting YAML files are intended for use with the remote MSA server's capabilities.
+    That is, --use_msa_server flag for boltz.
+    """
+    yaml_dict = {"version": 1, "sequences": []}  # initialize obj to populate
+    # Populate protein sequence
+    yaml_dict["sequences"].append(
+        {"protein": {"id": protein_id, "sequence": "".join(protein_sequence)}})
+    # Populate ligands smiles
+    for ligand_id, ligand_smiles in ligands_data.items():
+        yaml_dict["sequences"].append({"ligand": {"id": ligand_id, "smiles": ligand_smiles}})
+    # Dump data in yaml file
+    with open(output_yaml, "w") as out_file:
+        yaml.dump(yaml_dict, out_file)
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        description="CLI to write YAML files for boltz protein-ligand predictions.")
+    parser.add_argument("--protein-file", type=str, help="Path to protein PDB file.")
+    parser.add_argument("--ligand-file", type=str, help="Path to liband SDF file.")
+    parser.add_argument("--protein-id", type=str, help="Protein name/id.", default="protein")
+    parser.add_argument("--output-yaml", type=str, help="Path to yaml output.",
+                        default="ligand.yaml")
+    return parser
+
+
+def main():
+    parser = arg_parser()
+    args = parser.parse_args()
+    protein_sequence = sequence_from_pdb(args.protein_file)
+    ligands_data = ligands_data_from_sdf(args.ligand_file)
+    # Write yaml file
+    generate_boltz_yaml(protein_sequence, ligands_data, args.protein_id, args.output_yaml)
+
+
+if __name__ == "__main__":
+    main()
