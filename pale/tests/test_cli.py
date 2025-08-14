1ec91ee2cd2f28e6610eb19423ab87eb9fd486d3
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2025-08-14 17:37:13 -0400

    Adding test data. Using fixtures.

--- pale/tests/test_cli.py
+++ pale/tests/test_cli.py
@@ -9,12 +9,14 @@ from click.testing import CliRunner
 from pale.cli import mutate
 
 
-def test_mutate_from_pdb():
+def test_mutate_from_pdb(dipeptide_test_path, dipeptide_mutate_spec):
     """
     Test cli to generate protein mutation PDB
     """
     runner = CliRunner()
-    result = runner.invoke(mutate, args=f"--input-pdb {test_pdb} --mutation-spec {mut_spec}")
+    result = runner.invoke(
+        mutate, args=f"--input-pdb {dipeptide_test_path} --mutation-spec {dipeptide_mutate_spec}"
+    )
     assert result.exit_code == 0, (
         f"Command returned unsuccessfully with return code {result.exit_code}."
     )
